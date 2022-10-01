3684458865f873367afb412256a78b589e302335
MINEDUCYT <MINEDUCYT@DESKTOP-O47S48O>
2022-09-30 21:05:37 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/BLL/SedesBLL.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.BLL
+{
+    class SedesBLL
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Ubicacion { get; set; }
+    }
+}
---
+++ Ejercicio1/DAL/Database.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio1.Properties;
+using System.Windows.Forms;
+using System.Configuration;
+
+
+namespace Ejercicio1.DAL
+{
+    public class Database
+    {
+        public static string getStrConnection()
+        {
+            return Settings.Default.empleadosConnectionString;
+        }
+
+        public SqlConnection getConnection()
+        {
+            SqlConnection Con = new SqlConnection(getStrConnection());
+            return Con;
+        }
+    }
+}
